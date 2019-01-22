e29d2d997efcd0662fe3212fc51cb651aea3aa64
Khoa Nguyen <hi@khoanguyen.me>
2019-01-22 11:49:57 +0700

    Save it

--- new/src/forms/Form_ChildOIDP.js
+++ new/src/forms/Form_ChildOIDP.js
@@ -5,6 +5,7 @@ import { RadioGroup, SelectGroup, TextInput, DottedLabel } from "../components";
 import ButterToast, { Cinnamon } from "butter-toast";
 import MountPortal from "../MountPortal";
 import FormikAutosave from "../FormikAutosave";
+import { hoatDong, lietkeOptions } from "./Form_ChildOIDP_data";
 
 const blankInitialValues = {
   coKhoChiu: null,
@@ -35,43 +36,6 @@ const blankInitialValues = {
   "8-tansuat": null,
   "8-nguyennhan": [],
 };
-
-const lietkeOptions = [
-  { value: "1", label: "Đau răng, nhức răng" },
-  { value: "2", label: "Ê buốt răng khi ăn kem, khi uống nước đá, nước nóng" },
-  { value: "3", label: "Sâu răng, có lỗ trên răng" },
-  { value: "4", label: "Răng sữa bị lung lay, sắp rụng" },
-  {
-    value: "5",
-    label: "Trống răng (răng sữa rụng mà răng vĩnh viễn chưa mọc)",
-  },
-  { value: "6", label: "Răng vĩnh viễn bị gãy, vỡ lớn, mẻ" },
-  { value: "7", label: "Màu răng xấu, răng bị lốm đốm trắng đục" },
-  { value: "8", label: "Hình dạng răng không đẹp hay răng to hoặc nhỏ quá" },
-  {
-    value: "9",
-    label: "Vị trí của răng (ví dụ như bị cong hoặc bị nhô ra, hay có khe hở)",
-  },
-  { value: "10", label: "Chảy máu nướu khi chải răng" },
-  { value: "11", label: "Sưng nướu, đau nướu" },
-  {
-    value: "12",
-    label: "Vôi răng, răng có vôi đen hay bựa thức ăn bám trên răng",
-  },
-  { value: "13", label: "Lở loét hay trầy xướt trong miệng" },
-  { value: "14", label: "Hôi miệng hay hơi thở hôi" },
-  { value: "15", label: "Có tật ở vùng miệng hay mặt (sứt môi, hở hàm ếch)" },
-  { value: "16", label: "Răng đang mọc gây đau" },
-  {
-    value: "17",
-    label:
-      "Thiếu răng vĩnh viễn (răng vĩnh viễn không mọc) làm ăn nhai khó hay mặt không đẹp",
-  },
-  {
-    value: "99",
-    label: "Những khó chịu khác (ghi rõ)",
-  },
-];
 
 const lietkeName = "lietke";
 const lietkeCustomName = "lietkeCustom";
@@ -122,37 +86,6 @@ const Part2 = ({ setFieldValue, lietke, lietkeCustom }) => {
     </Section>
   );
 };
-
-const hoatDong = [
-  {
-    value: "1",
-    label: "Ăn nhai",
-    secondaryLabel: "(trong các bữa ăn,ăn kem,...)",
-  },
-  { value: "2", label: "Nói, phát âm" },
-  {
-    value: "3",
-    label: "Vệ sinh răng miệng",
-    secondaryLabel: "(như súc miệng, chải răng,...)",
-  },
-  { value: "4", label: "Nghỉ ngơi", secondaryLabel: "(kể cả việc ngủ)" },
-  {
-    value: "5",
-    label: "Tinh thần",
-    secondaryLabel: "(vui, buồn, hờn giận, bực tức, chán,...)",
-  },
-  { value: "6", label: "Cười", secondaryLabel: "(không thấy ngại ngùng)" },
-  {
-    value: "7",
-    label: "Học tập",
-    secondaryLabel: "(đi học, học tại lớp, làm bài tập ở nhà)",
-  },
-  {
-    value: "8",
-    label: "Tiếp xúc mọi người",
-    secondaryLabel: "(đi chơi với bạn bè, đến nhà bạn chơi)",
-  },
-];
 
 const levelOptions = [
   { label: "0", value: "0" },
@@ -285,8 +218,7 @@ const raiseError = (title, content) => {
 };
 
 const findLabelFromId = id => {
-  const row = lietkeOptions.find(row => row.value === id);
-  console.log(id);
+  const row = hoatDong.find(row => row.value === id);
   return row.label;
 };
 
@@ -317,37 +249,46 @@ const handleValidation = values =>
       const valueTansuat = processedValues[keyTansuat];
       const valueNguyenNhan = processedValues[keyNguyenNhan];
 
+      const label = findLabelFromId(String(i));
+
       /*
-       * Nguyen nhan
+       * Nguyên nhân
        */
-      // Có mức độ mà chưa đánh tần suất
-      if (valueMucdo !== 0 && valueTansuat === 0) {
-        const label = findLabelFromId(String(i));
-        raiseError("Thiếu tần suất", label);
+      if (valueMucdo === null) {
+        raiseError("Chọn mức trầm trọng", label);
         return reject();
       }
-      // Reset tần suất về 0 khi mức độ về 0
-      else if (valueMucdo === 0 && valueTansuat !== 0) {
-        processedValues[keyTansuat] = 0;
-      }
-      // Có mức độ và tần số mà chưa đánh nguyên nhân
-      else if (
-        valueMucdo !== 0 &&
-        valueTansuat !== 0 &&
-        valueNguyenNhan.length === 0
-      ) {
-        const label = findLabelFromId(String(i));
-        raiseError("Thiếu nguyên nhân", label);
+      // Có mức độ, thiếu tần suất
+      if (valueTansuat === 0) {
+        raiseError("Chọn tần suất", label);
+        // Reset tần suất, nguyên nhân khi mức độ = 0
+        processedValues[keyTansuat] = "0";
+        processedValues[keyNguyenNhan] = [];
         return reject();
-      } else {
-        const newNguyenNhan = valueNguyenNhan.filter(
-          nguyennhan => cacKhoChiu.indexOf(nguyennhan) !== -1
-        );
-        processedValues[keyNguyenNhan] = newNguyenNhan;
-        newNguyenNhan.forEach(nguyennhan => {
-          khoChiu_tuBangNguyenNhan.add(nguyennhan);
-        });
       }
+      // Có mức độ và tần số, thiếu  nguyên nhân
+      if (valueNguyenNhan.length === 0) {
+        const label = findLabelFromId(String(i));
+        raiseError("Chọn nguyên nhân", label);
+        return reject();
+      }
+
+      /*
+       * Đảm bảo các nguyên nhân của hoạt động này
+       * chỉ nằm trong các nguyên nhân được liệt kê
+       * Tránh trường hợp chọn ở liệt kê => chọn nguyên nhân
+       * => bỏ chọn liệt kê => nguyên nhân chưa được bỏ chọn
+       */
+      const newNguyenNhan = valueNguyenNhan.filter(
+        nguyennhan => cacKhoChiu.indexOf(nguyennhan) !== -1
+      );
+
+      processedValues[keyNguyenNhan] = newNguyenNhan;
+
+      // Thỏa mọi điều kiện, thêm nguyên nhân vào Set
+      newNguyenNhan.forEach(nguyennhan => {
+        khoChiu_tuBangNguyenNhan.add(nguyennhan);
+      });
     }
 
     /* Các khó chịu đã chọn ở trên mà chưa được đánh vào mục nguyên nhân */
---
+++ new/src/forms/Form_ChildOIDP_data.js
@@ -0,0 +1,67 @@
+export const lietkeOptions = [
+  { value: "1", label: "Đau răng, nhức răng" },
+  { value: "2", label: "Ê buốt răng khi ăn kem, khi uống nước đá, nước nóng" },
+  { value: "3", label: "Sâu răng, có lỗ trên răng" },
+  { value: "4", label: "Răng sữa bị lung lay, sắp rụng" },
+  {
+    value: "5",
+    label: "Trống răng (răng sữa rụng mà răng vĩnh viễn chưa mọc)",
+  },
+  { value: "6", label: "Răng vĩnh viễn bị gãy, vỡ lớn, mẻ" },
+  { value: "7", label: "Màu răng xấu, răng bị lốm đốm trắng đục" },
+  { value: "8", label: "Hình dạng răng không đẹp hay răng to hoặc nhỏ quá" },
+  {
+    value: "9",
+    label: "Vị trí của răng (ví dụ như bị cong hoặc bị nhô ra, hay có khe hở)",
+  },
+  { value: "10", label: "Chảy máu nướu khi chải răng" },
+  { value: "11", label: "Sưng nướu, đau nướu" },
+  {
+    value: "12",
+    label: "Vôi răng, răng có vôi đen hay bựa thức ăn bám trên răng",
+  },
+  { value: "13", label: "Lở loét hay trầy xướt trong miệng" },
+  { value: "14", label: "Hôi miệng hay hơi thở hôi" },
+  { value: "15", label: "Có tật ở vùng miệng hay mặt (sứt môi, hở hàm ếch)" },
+  { value: "16", label: "Răng đang mọc gây đau" },
+  {
+    value: "17",
+    label:
+      "Thiếu răng vĩnh viễn (răng vĩnh viễn không mọc) làm ăn nhai khó hay mặt không đẹp",
+  },
+  {
+    value: "99",
+    label: "Những khó chịu khác (ghi rõ)",
+  },
+];
+
+export const hoatDong = [
+  {
+    value: "1",
+    label: "Ăn nhai",
+    secondaryLabel: "(trong các bữa ăn, ăn kem,...)",
+  },
+  { value: "2", label: "Nói, phát âm" },
+  {
+    value: "3",
+    label: "Vệ sinh răng miệng",
+    secondaryLabel: "(như súc miệng, chải răng,...)",
+  },
+  { value: "4", label: "Nghỉ ngơi", secondaryLabel: "(kể cả việc ngủ)" },
+  {
+    value: "5",
+    label: "Tinh thần",
+    secondaryLabel: "(vui, buồn, hờn giận, bực tức, chán,...)",
+  },
+  { value: "6", label: "Cười", secondaryLabel: "(không thấy ngại ngùng)" },
+  {
+    value: "7",
+    label: "Học tập",
+    secondaryLabel: "(đi học, học tại lớp, làm bài tập ở nhà)",
+  },
+  {
+    value: "8",
+    label: "Tiếp xúc mọi người",
+    secondaryLabel: "(đi chơi với bạn bè, đến nhà bạn chơi)",
+  },
+];

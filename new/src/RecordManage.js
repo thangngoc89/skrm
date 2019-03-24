00b758d1d98562dbb907ab554695eb80c00f9c38
Khoa Nguyen <hi@khoanguyen.me>
2019-03-24 10:16:23 +0700

    Before refactoring into hooks

--- new/src/RecordManage.js
+++ new/src/RecordManage.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect, useReducer } from "react";
 import { Box, Heading, Text, Button, Layer } from "grommet";
 import db from "./db";
 import { navigate } from "@reach/router";

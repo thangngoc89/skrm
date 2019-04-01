1e60d4c85441980612ebff843b803ca49ed7cd1b
Khoa Nguyen <hi@khoanguyen.me>
2019-04-02 02:56:19 +0700

    fix: Make RecordEdit not live

--- new/src/RecordEdit.js
+++ new/src/RecordEdit.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { useGet } from "react-pouchdb";
+import React, { useEffect, useState } from "react";
+import { useDB } from "react-pouchdb";
 import RecordInput from "./RecordInput";
 
+let doc;
+
+const useGet = recordId => {
+  const db = useDB();
+
+  if (!doc || doc._id !== recordId) {
+    let promise = db
+      .get(recordId)
+      .then(result => {
+        doc = result;
+      })
+      .catch(console.error);
+
+    throw promise;
+  } else {
+    return doc;
+  }
+};
+
 const RecordEdit = ({ recordId }) => {
-  const doc = useGet({ id: recordId });
+  const doc = useGet(recordId);
 
   if (!doc) {
     return "Không tìm thấy hồ sơ với id " + recordId;
   } else {
-    return <RecordInput value={doc} />;
+    return <RecordInput key={recordId} value={doc} />;
   }
 };
 

936969bdb9175b3fded13f7fa422d0f00e5b39f3
Khoa Nguyen <hi@khoanguyen.me>
2021-03-21 21:56:40 +0700

    Update survey's sticky header

--- v3/src/routes/tieu-hoc.tsx
+++ v3/src/routes/tieu-hoc.tsx
@@ -1,29 +1,21 @@
-import { FunctionalComponent, h } from "preact";
+import { h } from "preact";
 import style from "./tieu-hoc.css";
 import { FormRenderer } from "../components/form/FormRender";
-import { ButtonGroup, Button, GridContainer, Grid } from "@trussworks/react-uswds";
+import { ButtonGroup, Button } from "@trussworks/react-uswds";
 // import { Link } from "preact-router/match";
 import { form as tieu_hoc_form } from "../components/form_schema/tieu_hoc_form";
 
-const Tieuhoc: FunctionalComponent = () => {
+const Tieuhoc: React.FC<{}> = () => {
   return (
     <div>
       <div className={style.sticky}>
-        <div className={style.left}>
-          <ButtonGroup type="segmented">
-            <Button type="button">Phiếu điều tra</Button>
-            <Button type="button" outline>
-              Bảng câu hỏi
-            </Button>
-            <Button type="button" outline>
-              Child-OIDP
-            </Button>
-          </ButtonGroup>
-        </div>
+        <nav className={style.left}>
+          <a href="#">Phiếu điều tra</a>
+          <a href="#">Bảng câu hỏi</a>
+          <a href="#">Child-OIDP</a>
+        </nav>
         <div className={style.right}>
-          <Button type="button" secondary>
-            Lưu
-          </Button>
+          <button>Lưu</button>
         </div>
       </div>
       <FormRenderer form={tieu_hoc_form} />

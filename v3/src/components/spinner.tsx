653ce9652e762e9249e7df7dfc1b233dddba8a73
Khoa Nguyen <hi@khoanguyen.me>
2021-03-23 23:20:32 +0700

    feat(ux): Delay Spinner by 400ms To prevent flashes of spinner

--- v3/src/components/spinner.tsx
+++ v3/src/components/spinner.tsx
@@ -1,6 +1,20 @@
 import { h } from "preact";
+import { useState, useEffect } from "react";
 import style from "./spinner.css";
+import objStr from "obj-str";
 
 export const Spinner: React.FC<{}> = () => {
-  return <div className={style.loader}></div>;
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <div
+      className={objStr({
+        [style.spinner]: true,
+        [style.delay]: isMounted,
+      })}
+    ></div>
+  );
 };

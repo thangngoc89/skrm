e00f8284573b577d9a6949e189b29ad58adbf433
Khoa Nguyen <hi@khoanguyen.me>
2021-05-06 15:13:44 +0700

    Move some files

--- v3/data_downloader/downloader/withStartAt.js
+++ v3/data_downloader/downloader/withStartAt.js
@@ -1,27 +1,27 @@
 import "isomorphic-fetch";
-import { Database } from "./firebase-firestore-lite/Database.js";
+import { Database } from "../firebase-firestore-lite/Database.js";
 import { createWriteStream } from "fs";
 import { join } from "path";
 
 const db = new Database({ projectId: "thucdia-e52ff" });
 
 const loadCollection = async (collectionName) => {
-  const ws = createWriteStream(join("/Users/khoa/dev/voser/v3/data_downloader/data", collectionName));
+  const ws = createWriteStream(join("/Users/khoa/dev/voser/v3/data_downloader/data", collectionName), { flags: "a" });
+
+  const docRef = db.ref("prod--surveyData/01F2MXT3PZ5VMC3ZV8DSESEVEQ");
+  const doc = await docRef.get();
 
   const collRef = db.ref(collectionName);
+  let res = await collRef.query({ startAt: doc }).run();
 
-  let counter = 0;
-  let list = await collRef.list();
-  counter = list.documents.length;
-  console.log("Downloaded", counter, "from", collectionName);
-  writeList(ws, list);
-
-  while (typeof list.options.pageToken !== "undefined") {
-    list = await list.getNextPage();
-    counter = counter + list.documents.length;
-    console.log("Downloaded", counter, "from", collectionName);
-    writeList(ws, list);
-  }
+  res
+    .map((doc) => {
+      return { __id: getDocumentId(doc), ...doc };
+    })
+    .forEach((doc) => {
+      ws.write(JSON.stringify(doc));
+      ws.write("\n");
+    });
 
   ws.close();
 };
